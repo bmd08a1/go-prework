fc42feb7e6e1070f8ee74e1f2b75ef17cbe3813c
Map Bui <bmd08a1@gmail.com>
2019-04-11 00:14:15 +0700

    get command line inputs

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"flag"
 )
 
 func main() {
-	fmt.Println("na ni?")
+	num_requests := flag.Int64("n", 1, "Number of requests to perform")
+	concurrency_level := flag.Int64("c", 1, "Number of multiple requests to make at a time")
+	flag.Parse()
+	link := flag.Args()[0]
+
+	fmt.Printf("number of requests: %v\n", *num_requests)
+	fmt.Printf("concurrency level: %v\n", *concurrency_level)
+	fmt.Printf("link: %v\n", link)
 }
